f7ff22375d48184880ad33568966c184ebf0516a
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2023-11-12 18:05:23 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -4,20 +4,20 @@ import numpy as np
 import streamlit as st
 import requests
 
+# Funct to get data from url
+def get_data(url):
+    req = requests.get(url)
+    if req.status_code == requests.codes.ok:
+        req = req.json()  # the response is a JSON
+        # req is now a dict with keys: name, encoding, url, size ...
+        # and content. But it is encoded with base64.
+        content = base64.decodestring(req['content'])
+        return(content)
+    else:
+        print('Content was not found.')
+
 # Read dataframe
-url = "https://github.com/Kingki19/DSI-Playground---Harga-Pangan/raw/main/data_harga.xlsx"
-response = requests.get(url)
-
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Save the content to a local file
-    with open("data_harga.xlsx", "wb") as f:
-        f.write(response.content)
-
-    # Now read the local file with pandas
-    data_harga = pd.read_excel("data_harga.xlsx")
-else:
-    print(f"Failed to download the file. Status code: {response.status_code}")
+data_harga = get_data("https://api.github.com/Kingki19/DSI-Playground---Harga-Pangan/raw/main/data_harga.xlsx")
 
 # Tes
 st.title("Visualization data and prediction for \'DSI-Playground Harga Pangan\' Competition")
