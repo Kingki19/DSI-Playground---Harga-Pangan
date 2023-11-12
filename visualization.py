b1dcbb30ce6cf07c07784c03833cf71163a8c15f
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2023-11-12 20:52:19 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -9,9 +9,18 @@ def get_df_from_url(url):
         df = pd.read_csv(url, index_col=0)
         return(df)
 
-# Read dataframe
-data_harga = get_df_from_url('https://raw.githubusercontent.com/Kingki19/DSI-Playground---Harga-Pangan/main/data_harga.csv')
+# Read dataframe from csv file
+data_harga_df = get_df_from_url('https://raw.githubusercontent.com/Kingki19/DSI-Playground---Harga-Pangan/main/data_harga.csv')
+bm_summary_df = get_df_from_url('https://raw.githubusercontent.com/Kingki19/DSI-Playground---Harga-Pangan/main/bm_summary_pred.csv')
+dar_summary_df = get_df_from_url('https://raw.githubusercontent.com/Kingki19/DSI-Playground---Harga-Pangan/main/dar_summary_pred.csv')
+bp_summary_df = get_df_from_url('https://raw.githubusercontent.com/Kingki19/DSI-Playground---Harga-Pangan/main/bp_summary_pred.csv')
 
 # Tes
 st.title("Visualization data and prediction for \'DSI-Playground Harga Pangan\' Competition")
-st.dataframe(data_harga)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
