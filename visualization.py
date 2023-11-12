6e55da9e59881c683a97cdda24ba6908a2036bb6
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2023-11-12 17:45:22 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,1 +1,11 @@
+# Import library
+import pandas as pd
+import numpy as np
+import streamlit as st
 
+# Read dataframe
+data_harga = pd.read_csv("data_harga.xlsx")
+
+# Tes
+st.title("Visualization data and prediction for \'DSI-Playground Harga Pangan\' Competition")
+st.dataframe(data_harga)
