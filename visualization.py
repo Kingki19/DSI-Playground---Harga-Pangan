da9f74fb57a12c24a32f75401ae6a8be4be09a7c
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2023-11-12 17:51:51 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -4,7 +4,7 @@ import numpy as np
 import streamlit as st
 
 # Read dataframe
-data_harga = pd.read_excel("DSI-Playground---Harga-Pangan/data_harga.xlsx")
+data_harga = pd.read_excel("https://github.com/Kingki19/DSI-Playground---Harga-Pangan/blob/main/data_harga.xlsx")
 
 # Tes
 st.title("Visualization data and prediction for \'DSI-Playground Harga Pangan\' Competition")
